00ab778470f1ed5a140553c64e886407b9cc5eaf
yosukedoke <yd.niku@gmail.com>
2014-06-26 22:09:58 +0900

    表紙と目次の出力

--- src/js/HTML5-Documents.js
+++ src/js/HTML5-Documents.js
@@ -2,6 +2,26 @@
   'use strict';
 
   var doc = window.document;
+
+  var TMPL_COVER_PAGE = '<article class="cover">' +
+      '<h1><%= title %></h1>' +
+      '<time class="last-modified"><%= time %></time>' +
+      '<address>© 2014 <%= author %></address>' +
+      '</article>';
+  var TMPL_INDEX_PAGE = '<article class="index">' +
+      //'</header>' +
+      '<main>' +
+      '<h1>Index</h1>' +
+      '<ol>' +
+      '<% for (var i = 0;i < index.length;i++){ %>' +
+        '<li><a href="#<%= index[i].id %>"><%= index[i].title %></a></li>' +
+      '<% } %>' +
+      '</ol>' +
+      '</main>' +
+      //'<footer>' +
+      //'<address>© 2014 <%= author %></address>' +
+      //'</footer>' +
+      '</article>';
   var TMPL_HEADER =
       '<header>' +
       '<h1><%= title %></h1>' +
@@ -13,10 +33,18 @@
       '<address>© 2014 <%= author %></address>' +
       '</footer>';
 
+  var TMPL_LOCAL_FOOTER =
+      '<footer>' +
+      '<address>© 2014 <%= author %></address>' +
+      '<p class="nombre"><%= page %></p>' +
+      '</footer>';
+
   function init() {
     if(marked && typeof(marked) === 'function') {
       replaceMarkdownText();
     }
+
+    var $main = $('main');
 
     var time   = $(doc).find('body > time').text();
     var title  = $(doc).find('head > title').text();
@@ -27,15 +55,39 @@
 
     $(doc.body).prepend(header, footer);
 
-    $('main > article').each(function() {
-      $(this).prepend(header).append(footer);
+
+    var index = [];
+    $('main > article').each(function(i) {
+      var $this = $(this);
+      var title = $this.find('h1').text();
+      var id = $this.find('h1').attr('id') || 'page' + i;
+      index.push({
+        title : title,
+        id: id
+      });
+      $this.find('h1').attr({'id': id});
     });
+
+    var indexPage = new EJS({text:TMPL_INDEX_PAGE}).render({title: title, time: time, author: author, index: index});
+    $main.prepend(indexPage);
+
+    $('main > article').each(function(i) {
+      var $this = $(this);
+      var localFooter = new EJS({text:TMPL_LOCAL_FOOTER}).render({author: author, page: i + 1});
+      $this.prepend(header).append(localFooter);
+    });
+
+    var coverPage = new EJS({text:TMPL_COVER_PAGE}).render({title: title, time: time, author: author});
+
+    $main.prepend(coverPage);
+
   }
 
   function replaceMarkdownText() {
     // TODO: highlight.js 使ってみる https://github.com/isagalaev/highlight.js
     $(doc).find('.marked').each(function() {
-      var text = $(this).html();
+      var $this = $(this);
+      var text = $this.html();
       $this.empty().append(marked(text)).removeClass('marked');
     });
   }
